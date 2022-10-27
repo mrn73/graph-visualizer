217eefc15b3452663e1ae58d2e3491d9ab7e9ea8
Michael Nelli <mrn73@scarletmail.rutgers.edu>
2022-10-27 13:49:16 -0400

    Reworking grid from table to css grid.

--- src/components/grid.js
+++ src/components/grid.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useReducer } from 'react';
 import { NodeType } from '../node.js';
 import './grid.css';
 
+/*
 function Grid(props) {
 	const [mouseState, setMouseState] = useState({isHeld: false});
 
@@ -44,6 +45,45 @@ function Grid(props) {
 		</table>
 	);
 }
+*/
+
+function Grid(props) {
+	const [mouseState, setMouseState] = useState({isHeld: false});
+
+	const updateCell = (i, j, e) => {
+		props.updateGridCell(i, j, e, mouseState);
+	}
+
+	const handleMouseDown = (e) => {
+		setMouseState({isHeld: true, button: e.button});
+	}
+
+	const handleMouseUp = () => {
+		setMouseState({isHeld: false});
+	}
+	
+	return (
+		<div className={"gridContainer"}>
+			{props.grid.map((row, i) => 
+				row.map((_, j) => { 
+					let type;
+					if (props.start && i == props.start.i && j == props.start.j) {
+						type = NodeType.SRC;
+					} else if (props.end && i == props.end.i && j == props.end.j) {
+						type = NodeType.DST;
+					} else {
+						type = props.grid[i][j];
+					}
+					return (
+						<Cell key={i * 60 + j} value={{row: i, col: j, type}} 
+						handleAction={updateCell}/>
+					);
+				})
+			)}
+		</div>
+	);
+}
+
 
 function Cell(props) {
 	const handleMouseEvent = (e) => {
