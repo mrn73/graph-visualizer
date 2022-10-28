189a22c9cd62a158208ec2cadcf6850c5435a0f6
Michael Nelli <mrn73@scarletmail.rutgers.edu>
2022-10-28 02:05:09 -0400

    Grid scales with window.

--- src/components/grid.js
+++ src/components/grid.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useReducer } from 'react';
 import { NodeType } from '../node.js';
 import './grid.css';
 
-/*
 function Grid(props) {
 	const [mouseState, setMouseState] = useState({isHeld: false});
 
@@ -16,54 +15,13 @@ function Grid(props) {
 
 	const handleMouseUp = () => {
 		setMouseState({isHeld: false});
-	}
+	}	
+	//console.log(props.grid);
+	const cols = props.grid[0].length;
 	
 	return (
-		<table className={"gridGraph"} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
-			<tbody>
-				{props.grid.map((row, i) => 
-					<tr key={i}>
-						{row.map((_, j) => { 
-							let type;
-							if (props.start && i == props.start.i && j == props.start.j) {
-								type = NodeType.SRC;
-							} else if (props.end && i == props.end.i && j == props.end.j) {
-								type = NodeType.DST;
-							} else {
-								type = props.grid[i][j];
-							}
-							return (
-								<td key={j} className={"gridGraphCell"}>
-									<Cell value={{row: i, col: j, type}} 
-									handleAction={updateCell}/>
-								</td>
-							)
-						})}
-					</tr>
-				)}
-			</tbody>
-		</table>
-	);
-}
-*/
-
-function Grid(props) {
-	const [mouseState, setMouseState] = useState({isHeld: false});
-
-	const updateCell = (i, j, e) => {
-		props.updateGridCell(i, j, e, mouseState);
-	}
-
-	const handleMouseDown = (e) => {
-		setMouseState({isHeld: true, button: e.button});
-	}
-
-	const handleMouseUp = () => {
-		setMouseState({isHeld: false});
-	}
-	
-	return (
-		<div className={"gridContainer"}>
+		<div className={"grid"} 
+		     style={{gridTemplateColumns: "repeat(" + cols + ", 1fr)"}}>
 			{props.grid.map((row, i) => 
 				row.map((_, j) => { 
 					let type;
@@ -75,7 +33,7 @@ function Grid(props) {
 						type = props.grid[i][j];
 					}
 					return (
-						<Cell key={i * 60 + j} value={{row: i, col: j, type}} 
+						<Cell key={i * cols + j} value={{row: i, col: j, type}} 
 						handleAction={updateCell}/>
 					);
 				})
