2131c8604038f3148d612aadc978313f9a5431c2
Michael Nelli <mrn73@scarletmail.rutgers.edu>
2022-11-27 17:27:45 -0500

    Minor visual changes.

--- src/node.js
+++ src/node.js
@@ -1,4 +1,4 @@
-/* Immutable enum */
+/* Immutable enum 
 export const NodeType = Object.freeze({
 	NORMAL: 1,
 	BLOCKED: 2,
@@ -14,35 +14,52 @@ export const NodeType = Object.freeze({
 	ROCK: 12,
 	SNOW: 13
 });
+*/
+
+export const NodeType = Object.freeze({
+	NORMAL: 1,
+	DEEPWATER: 2,
+	WATER: 3,
+	SAND: 4,
+	FOREST: 5,
+	GRASSLAND: 6,
+	ROCK: 7,
+	SNOW: 8,
+	BLOCKED: 9,
+	SRC: 10,
+	DST: 11,
+	VISITED: 12,
+	PATH: 13
+});
 
 export function getName(t) {
 	switch (t) {
 		case 1:
 			return "Normal";
 		case 2:
-			return "Blocked";
-		case 3:
-			return "Source";
-		case 4:
-			return "Destination";
-		case 5:
-			return "Visited";
-		case 6:
-			return "Path";
-		case 7:
 			return "Deep Water";
-		case 8:
+		case 3:
 			return "Water";
-		case 9:
+		case 4:
 			return "Sand";
-		case 10:
+		case 5:
 			return "Forest";
-		case 11:
+		case 6:
 			return "Grassland";
-		case 12:
+		case 7:
 			return "Rock";
-		case 13:
+		case 8:
 			return "Snow";
+		case 9:
+			return "Wall";
+		case 10:
+			return "Source";
+		case 11:
+			return "Destination";
+		case 12:
+			return "Visited";
+		case 13:
+			return "Path";
 		default:
 			return "Undefined";
 	}
